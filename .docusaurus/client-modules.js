231bb2460be17411b214f5b887568fda2ab967f6
jimken-mido <jimken-mido@users.noreply.github.com>
2025-12-15 18:15:43 +0000

    deploy: 8085c4d5e0ff79b75123c1774c51528bc8dd0a44

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/jim/Desktop/github/my-website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/jim/Desktop/github/my-website/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/jim/Desktop/github/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/jim/Desktop/github/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/jim/Desktop/github/my-website/src/css/custom.css"),
-];
